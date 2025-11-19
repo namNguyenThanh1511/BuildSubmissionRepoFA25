4e07ecd9877cab1bd4099a766cd4678ccb8904d9
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-19 21:26:15 +0700

    Add submission: andntse184673 - 2025-11-19 21:26:15

---
+++ submissions/andntse184673-20251119212615-0/solution/PRN232_SU25_SE184673.Repository/Repositories/LeopardAccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRN232_SU25_SE184673.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_SE184673.Repository.Repositories
+{
+    public class LeopardAccountRepository
+    {
+        private readonly SU25LeopardDBContext _dbContext;
+
+        public LeopardAccountRepository(SU25LeopardDBContext dbContext) { _dbContext = dbContext; }
+
+        public async Task<LeopardAccount?> Authentication(string email, string password)
+        {
+            var account = await _dbContext.LeopardAccounts
+                .FirstOrDefaultAsync(a => a.Email == email && a.Password == password);
+            if (account == null) return null;
+            return account;
+        }
+    }
+}
