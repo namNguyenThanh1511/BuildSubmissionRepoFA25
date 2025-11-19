f927e83b1674e4e467f953ad0aa93aa7bd4e466b
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-19 21:29:02 +0700

    Add submission: andntse184673 - 2025-11-19 21:29:02

---
+++ submissions/andntse184673-20251119212901-0/PRN232_SU25_SE184673.api/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRN232_SU25_SE184673.Service;
+
+namespace PRN232_SU25_SE184673.api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private LeopardAccountService _accountService;
+        private JwtService _jwtService;
+        public AccountController(LeopardAccountService accountService, JwtService jwtService) 
+        { 
+            _accountService = accountService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authentication(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email)) return StatusCode(400, new
+            {
+                errorMessage = "Email must not null or empty"
+            });
+            if (string.IsNullOrEmpty(password)) return StatusCode(400, new
+            {
+                errorMessage = "Password must not null or empty"
+            });
+            var acc = await _accountService.Authentication(email, password);
+            if (acc == null) return StatusCode(404);
+            return Ok(_jwtService.GenerateToken(acc));
+            
+        }
+    }
+}
