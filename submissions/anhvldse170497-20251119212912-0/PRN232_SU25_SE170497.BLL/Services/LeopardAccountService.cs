b2d86b00878102e53994d1bc2e1000a3c11dcb4d
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-19 21:29:12 +0700

    Add submission: anhvldse170497 - 2025-11-19 21:29:12

---
+++ submissions/anhvldse170497-20251119212912-0/PRN232_SU25_SE170497.BLL/Services/LeopardAccountService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PRN232_SU25_SE170497.DAL.ModelExtensions;
+using PRN232_SU25_SE170497.DAL.Models;
+using PRN232_SU25_SE170497.DAL.Repositories;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BLL.Services
+{
+    public class LeopardAccountService : ILeopardAccountService
+    {
+        private readonly GenericRepository<LeopardAccount> _genericRepository;
+        private readonly IConfiguration _config;
+
+        public LeopardAccountService(GenericRepository<LeopardAccount> genericRepository, IConfiguration configuration)
+        {
+            _genericRepository = genericRepository;
+            _config = configuration;
+        }
+
+        public async Task<Result<TokenResponse>> LoginAsync(string email, string password)
+        {
+            var user = (await _genericRepository.FindWithIncludeAsync(
+                predicate: query => query.Email == email && query.Password == password)).FirstOrDefault();
+
+            if (user == null)
+            {
+                return Errors.ValidationError<TokenResponse>("Invalid email or password");
+            }
+
+            var token = GenerateAccessToken(user);
+
+            string roleName = user.RoleId switch
+            {
+                5 => "admin",
+                6 => "moderator",
+                7 => "developer",
+                4 => "member",
+                _ => "unknown"
+            };
+
+            var tokenResponse = new TokenResponse(Token: token, Role: roleName);
+
+            return Result<TokenResponse>.Ok(tokenResponse);
+        }
+
+        private string GenerateAccessToken(LeopardAccount account)
+        {
+            try
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:SecretKey"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(_config["JwtSettings:Issuer"]
+                        , _config["JwtSettings:Audience"]
+                        , new Claim[]
+                        {
+            new(ClaimTypes.Name, account.UserName),
+            new(ClaimTypes.Email, account.Email),
+            new Claim(ClaimTypes.Role, account.RoleId.ToString())
+                        },
+                        expires: DateTime.Now.AddMinutes(120),
+                        signingCredentials: credentials
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return tokenString;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+
+    public record TokenResponse(string Token, string Role);
+}
