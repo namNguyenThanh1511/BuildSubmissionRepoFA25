b4883b617c902b80dd6f0ebfb32515590b62ab98
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-20 00:58:45 +0700

    Add submission: annqkhe180905 - 2025-11-20 00:58:45

---
+++ submissions/annqkhe180905-20251120005845-0/PRN231_SU25_HE180905.api/PRN231_SU25_HE180905.api/DataAccessLayer/ReqAndRes/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ReqAndRes
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
---
+++ submissions/annqkhe180905-20251120005845-0/PRN231_SU25_HE180905.api/PRN231_SU25_HE180905.api/PRN231_SU25_HE180905.api/Program.cs
@@ -0,0 +1,123 @@
+﻿using BusinessLogicLayer.Interface;
+using BusinessLogicLayer.Service;
+using DataAccessLayer;
+using DataAccessLayer.Interface;
+using DataAccessLayer.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<Su25leopardDbContext> (options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var keyBytes = Encoding.UTF8.GetBytes(jwtSection["Key"]);
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtSection["Issuer"],
+
+            ValidateAudience = true,
+            ValidAudience = jwtSection["Audience"],
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddControllers()
+    .AddOData(opt =>
+    {
+        var odataBuilder = new ODataConventionModelBuilder();
+        odataBuilder.EntitySet<LeopardProfile>("LeopardProfile");
+        opt.Select().Filter().OrderBy().Expand().Count().SetMaxTop(100);
+        opt.AddRouteComponents("odata", odataBuilder.GetEdmModel());
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("FullAccess", policy =>
+        policy.RequireRole("administrator", "moderator"));
+    options.AddPolicy("ReadOnly", policy =>
+        policy.RequireRole("developer", "member", "administrator", "moderator"));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(opts =>
+{
+    opts.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    opts.JsonSerializerOptions.MaxDepth = 64;
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter: Bearer {token}"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id   = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
