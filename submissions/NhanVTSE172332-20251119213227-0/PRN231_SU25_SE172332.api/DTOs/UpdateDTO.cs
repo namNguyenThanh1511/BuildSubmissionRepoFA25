fe3daf9b21f33d086092db0c0cea3449b0dfa6ae
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-19 21:32:28 +0700

    Add submission: NhanVTSE172332 - 2025-11-19 21:32:27

---
+++ submissions/NhanVTSE172332-20251119213227-0/PRN231_SU25_SE172332.api/DTOs/UpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace PRN231_SU25_SE172332.api.DTOs
+{
+    public class UpdateDto
+    {
+		public int? LeopardTypeId { get; set; }
+
+		public string? LeopardName { get; set; }
+
+		public double? Weight { get; set; }
+
+		public string? Characteristics { get; set; }
+
+		public string? CareNeeds { get; set; }
+
+	}
+}
---
+++ submissions/NhanVTSE172332-20251119213227-0/Repositories/LeopardAccountRepo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Base;
+using Repositories.Models;
+
+namespace Repositories
+{
+	public class LeopardAccountRepo : GenericRepository<LeopardAccount>
+	{
+		public LeopardAccountRepo() { }
+
+		public async Task<LeopardAccount> GetUserAccountAsync(string email, string password)
+		{
+			return await _context.LeopardAccounts.FirstOrDefaultAsync(u => u.Email.ToLower().Equals(email.ToLower())
+			&& u.Password.Equals(password));
+		}
+	}
+}
