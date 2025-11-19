672a8e984540e0f5d0e2de5154db1add03986a37
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-20 00:58:40 +0700

    Add submission: AnhNNTSE172020 - 2025-11-20 00:58:40

---
+++ submissions/AnhNNTSE172020-20251120005840-0/PRN231_SU25_SE172020.api/BusinessLogic/Service/ILeopardProfileService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Models;
+using DataAccess.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Service
+{
+    public interface ILeopardProfileService
+    {
+
+        Task<IEnumerable<LeopardProfile>> GetAllLeopardsAsync();
+        Task<LeopardProfile?> GetLeopardByIdAsync(int id);
+        Task<bool> CreateLeopardProfileAsync(LeopardProfileRequest leopardProfile);
+        Task<bool> UpdateLeopardProfileAsync(int id, LeopardProfileRequest leopardProfile);
+        Task<bool> DeleteLeopardProfileAsync(int id);
+        Task<IEnumerable<Object>> SearchLeopardProfileAsync(string? LeopardName, double? Weight);
+    }
+}
