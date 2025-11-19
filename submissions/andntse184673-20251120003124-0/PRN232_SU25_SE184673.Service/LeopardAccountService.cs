791e31fe6aa0b9419cf7f9343059cf1207769fab
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-20 00:31:24 +0700

    Add submission: andntse184673 - 2025-11-20 00:31:24

---
+++ submissions/andntse184673-20251120003124-0/PRN232_SU25_SE184673.Service/LeopardAccountService.cs
@@ -0,0 +1,22 @@
+﻿using PRN232_SU25_SE184673.Repository.Models;
+using PRN232_SU25_SE184673.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN232_SU25_SE184673.Service
+{
+    public class LeopardAccountService
+    {
+        private readonly LeopardAccountRepository _repository;
+
+        public LeopardAccountService (LeopardAccountRepository service) {_repository = service; }
+
+        public async Task<LeopardAccount> Authentication(string username, string password)
+        {
+            return await _repository.Authentication(username, password);
+        }
+    }
+}
